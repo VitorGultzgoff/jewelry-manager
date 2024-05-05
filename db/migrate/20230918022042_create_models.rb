53c7a18dac6bd0a6aff481fa912b1a6343b7cd8e
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 20:15:12 -0400

    feat: Include Model migration definition

---
+++ db/migrate/20230918022042_create_models.rb
@@ -0,0 +1,10 @@
+class CreateModels < ActiveRecord::Migration[7.0]
+  def change
+    create_table :models do |t|
+      t.string :name
+      t.string :icon_image_url
+
+      t.timestamps
+    end
+  end
+end
