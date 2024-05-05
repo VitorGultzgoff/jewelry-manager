08c9df8d3904efdd57b84f76da515b0f8273bcf1
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 20:57:51 -0400

    chore : Include generated comments on Model

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: models
+#
+#  id             :integer          not null, primary key
+#  name           :string
+#  icon_image_url :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Model < ApplicationRecord
   has_and_belongs_to_many :products
 end
