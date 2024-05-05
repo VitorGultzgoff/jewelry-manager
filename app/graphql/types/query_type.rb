c63fa07e932d946d5a5b3994db8a5a04d8aedd88
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 21:18:22 -0400

    chore: Include models and products method resolvers on query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,6 +4,9 @@ module Types
       argument :id, ID, required: true, description: "ID of the object."
     end
 
+    field :models, [Types::ModelType], null: false
+    field :products, [Types::ProductType], null: false
+
     def node(id:)
       context.schema.object_from_id(id, context)
     end
@@ -16,14 +19,12 @@ module Types
       ids.map { |id| context.schema.object_from_id(id, context) }
     end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    def models
+      Model.all
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def products
+      Product.all
     end
   end
 end
