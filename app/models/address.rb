46d03fa439a4c64ed2523f70e17e06fbe399c74f
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 22:20:53 -0400

    feat: Include addresses tables in the database generated comments

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id             :integer          not null, primary key
+#  street_address :string
+#  complement     :string
+#  zipcode        :string
+#  country_id     :integer          not null
+#  state_id       :integer          not null
+#  city_id        :integer          not null
+#  customer_id    :integer          not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Address < ApplicationRecord
   belongs_to :country
   belongs_to :state
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: states
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  country_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class State < ApplicationRecord
   belongs_to :country
 end
