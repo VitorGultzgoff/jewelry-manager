2a8e8393bf72ce594bb7c04ba1a8305b53f1b370
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 21:18:52 -0400

    chore: Remove node from query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,23 +1,7 @@
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
-    end
-
     field :models, [Types::ModelType], null: false
     field :products, [Types::ProductType], null: false
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
-    end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
 
     def models
       Model.all
