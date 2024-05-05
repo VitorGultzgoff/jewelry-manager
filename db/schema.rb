4d82ba716dc33aca483d798b4952b5129161f236
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 22:24:42 -0400

    feat: Include customers table in the database generated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_021829) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_05_022320) do
   create_table "addresses", force: :cascade do |t|
     t.string "street_address"
     t.string "complement"
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_05_021829) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "document"
+    t.string "avatar_url"
+    t.string "name"
+    t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
