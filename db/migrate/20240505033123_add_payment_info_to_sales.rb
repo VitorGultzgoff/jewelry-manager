0ba3aa7ea117290b09eccf0037e9f01887e09f7d
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-05-04 23:33:56 -0400

    chore: Include payment info into sales

---
+++ db/migrate/20240505033123_add_payment_info_to_sales.rb
@@ -0,0 +1,7 @@
+class AddPaymentInfoToSales < ActiveRecord::Migration[7.0]
+  def change
+    add_column :sales, :payment_method, :string
+    add_column :sales, :payment_status, :string
+    add_column :sales, :schedule_payment_date, :date
+  end
+end
