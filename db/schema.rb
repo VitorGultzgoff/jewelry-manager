f7c7b15929265e230b1ac96bf8cd96f8acbb5f0e
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-06-26 20:49:24 -0400

    chore: Include users table basic definition generated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_034855) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_05_192828) do
   create_table "addresses", force: :cascade do |t|
     t.string "street_address"
     t.string "complement"
@@ -138,6 +138,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_05_034855) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "encrypted_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "addresses", "cities"
