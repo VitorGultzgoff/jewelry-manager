8e8931cb525731e64ab863be4a68ebee8c781b80
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-07-26 00:01:16 -0400

    chore: Seed data of Products generated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_26_035436) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_26_035752) do
   create_table "addresses", force: :cascade do |t|
     t.string "street_address"
     t.string "complement"
