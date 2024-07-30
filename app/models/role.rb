e6cd818cc019ba15c7e11b11d6e31a3e10267c37
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-07-29 20:24:36 -0400

    chore: Include missing annotation comments on Role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Role < ApplicationRecord
   has_and_belongs_to_many :users
 
