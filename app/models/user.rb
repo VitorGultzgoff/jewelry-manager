4956e2db57648432515cdbc0b63e641036b6fc50
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-07-29 20:24:17 -0400

    chore: Include missing table annotation comments on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  name               :string
+#  email              :string
+#  encrypted_password :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 class User < ApplicationRecord
   has_and_belongs_to_many :roles
 
