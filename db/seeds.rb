ad17a46386e5a83113a21b5fb1af1208bf092340
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2024-11-10 02:46:11 -0500

    refactor: Switch seeds in migrations for proper seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,79 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed Brazilian states and cities
+brazil = Country.find_or_create_by(name: 'Brazil')
+
+states_with_cities = {
+  'Acre' => %w[Rio_Branco Cruzeiro_do_Sul Sena_Madureira],
+  'Alagoas' => %w[Maceió Arapiraca Palmeira_dos_Índios],
+  'Amapá' => %w[Macapá Santana Oiapoque],
+  'Amazonas' => %w[Manaus Parintins Itacoatiara],
+  'Bahia' => %w[Salvador Feira_de_Santana Vitória_da_Conquista],
+  'Ceará' => %w[Fortaleza Caucaia Juazeiro_do_Norte],
+  'Distrito_Federal' => %w[Brasília Ceilândia Taguatinga],
+  'Espírito_Santo' => %w[Vitória Vila_Velha Serra],
+  'Goiás' => %w[Goiânia Aparecida_de_Goiânia Anápolis],
+  'Maranhão' => %w[São_Luís Imperatriz Timon],
+  'Mato_Grosso' => %w[Cuiabá Várzea_Grande Rondonópolis],
+  'Mato_Grosso_do_Sul' => %w[Campo_Grande Dourados Três_Lagoas],
+  'Minas_Gerais' => %w[Belo_Horizonte Uberlândia Uberaba],
+  'Pará' => %w[Belém Ananindeua Santarém],
+  'Paraíba' => %w[João_Pessoa Campina_Grande Santa_Rita],
+  'Paraná' => %w[Curitiba Londrina Maringá],
+  'Pernambuco' => %w[Recife Jaboatão_dos_Guararapes Olinda],
+  'Piauí' => %w[Teresina Parnaíba Picos],
+  'Rio_de_Janeiro' => %w[Rio_de_Janeiro Niterói Petrópolis],
+  'Rio_Grande_do_Norte' => %w[Natal Mossoró Parnamirim],
+  'Rio_Grande_do_Sul' => %w[Porto_Alegre Caxias_do_Sul Pelotas],
+  'Rondônia' => %w[Porto_Velho Ji-Paraná Ariquemes],
+  'Roraima' => %w[Boa_Vista Rorainópolis Caracaraí],
+  'Santa_Catarina' => %w[Florianópolis Joinville Blumenau],
+  'São_Paulo' => %w[São_Paulo Campinas Santos],
+  'Sergipe' => %w[Aracaju Nossa_Senhora_do_Socorro Lagarto],
+  'Tocantins' => %w[Palmas Araguaína Gurupi]
+}
+
+states_with_cities.each do |state_name, cities|
+  state = brazil.states.find_or_create_by(name: state_name)
+  cities.each do |city_name|
+    state.cities.find_or_create_by(name: city_name)
+  end
+end
+
+puts 'Brazilian states and cities have been seeded successfully.'
+
+# Seed jewelry models
+models = %w[Chain Bracelet Choker Ring Pendant Earring]
+
+models.each do |model_name|
+  Model.find_or_create_by(name: model_name)
+end
+
+puts 'Jewelry models have been seeded successfully.'
+
+# Seed suppliers
+suppliers = %w[Haig Paulo Leilão_da_Caixa]
+
+suppliers.each do |supplier_name|
+  Supplier.find_or_create_by(name: supplier_name.gsub('_', ' '))
+end
+
+puts 'Suppliers have been seeded successfully.'
+
+# Seed products
+statuses = %w[Stock Consigned Sold Dismantled Lost Canceled]
+
+statuses.each do |status|
+  Model.find_each do |model|
+    Supplier.find_each do |supplier|
+      Product.create(
+        name: "#{model.name} #{status}",
+        status:,
+        cost_price: rand(100..500),
+        sale_price: rand(600..1000),
+        model:,
+        supplier:
+      )
+    end
+  end
+end
+
+puts 'Products have been seeded successfully.'
